5cd2320488346115232982c6af584101c393fc11
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2016-04-15 12:00:09 +0100

    Adding index page and adjusting Makefile for multi-note setup

--- build.R
+++ build.R
@@ -1,6 +1,13 @@
 library(rmarkdown)
 library(tufte)
-render('points.Rmd', 'tufte_html')
-render('points.Rmd', 'tufte_handout')
-file.rename('points.pdf', 'points_book.pdf')
+
+    render('index.Rmd', 'tufte_html')
+
+    render('points.Rmd', 'tufte_html')
+    render('points.Rmd', 'tufte_handout')
+    file.rename('points.pdf', 'points_book.pdf')
+
+    render('sp_eco.Rmd', 'tufte_html')
+    render('sp_eco.Rmd', 'tufte_handout')
+    file.rename('sp_eco.pdf', 'sp_eco_book.pdf')
 
