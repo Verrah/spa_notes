e4a04082868d6281a306292060eea98c595d9e4f
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2016-04-17 18:18:59 +0100

    Other minor changes

--- build.R
+++ build.R
@@ -1,11 +1,11 @@
 library(rmarkdown)
 library(tufte)
 
-#   render('index.Rmd', 'tufte_html')
+    render('index.Rmd', 'tufte_html')
 
-#   render('points.Rmd', 'tufte_html')
-#   render('points.Rmd', 'tufte_handout')
-#   file.rename('points.pdf', 'points_book.pdf')
+    render('points.Rmd', 'tufte_html')
+    render('points.Rmd', 'tufte_handout')
+    file.rename('points.pdf', 'points_book.pdf')
 
     render('sp_eco.Rmd', 'tufte_html')
     render('sp_eco.Rmd', 'tufte_handout')
