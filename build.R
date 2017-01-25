8e5ba51b7d4d1f40d73cca78b1f7f942dfa38b0f
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2017-01-25 07:09:58 +0000

    Adding css and updating build.R accordingly

--- build.R
+++ build.R
@@ -1,13 +1,13 @@
 library(rmarkdown)
 library(tufte)
 
-    render('index.Rmd', 'tufte_html')
+    #render('index.Rmd')
 
-    render('points.Rmd', 'tufte_html')
+    render('points.Rmd')
     render('points.Rmd', 'tufte_handout')
     file.rename('points.pdf', 'points_book.pdf')
 
-    render('sp_eco.Rmd', 'tufte_html')
-    render('sp_eco.Rmd', 'tufte_handout')
-    file.rename('sp_eco.pdf', 'sp_eco_book.pdf')
+    #render('sp_eco.Rmd')
+    #render('sp_eco.Rmd', 'tufte_handout')
+    #file.rename('sp_eco.pdf', 'sp_eco_book.pdf')
 
