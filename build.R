1084a3e24591af206b8c8d6cabc18b7935de4600
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2017-03-01 18:52:04 +0000

    Turning build off

--- build.R
+++ build.R
@@ -3,11 +3,15 @@ library(tufte)
 
     #render('index.Rmd')
 
-    render('points.Rmd')
-    render('points.Rmd', 'tufte_handout')
-    file.rename('points.pdf', 'points_book.pdf')
+    #render('points.Rmd')
+    #render('points.Rmd', 'tufte_handout')
+    #file.rename('points.pdf', 'points_book.pdf')
 
     #render('sp_eco.Rmd')
     #render('sp_eco.Rmd', 'tufte_handout')
     #file.rename('sp_eco.pdf', 'sp_eco_book.pdf')
+    
+    #render('flows.Rmd')
+    #render('flows.Rmd', 'tufte_handout')
+    #file.rename('flows.pdf', 'flows_book.pdf')
 
