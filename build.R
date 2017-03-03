d85e38455dff4d455862bf25112283c830b55252
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2017-03-03 09:52:59 +0000

    Turning on points build

--- build.R
+++ build.R
@@ -3,9 +3,9 @@ library(tufte)
 
     render('index.Rmd')
 
-    #render('points.Rmd')
-    #render('points.Rmd', 'tufte_handout')
-    #file.rename('points.pdf', 'points_book.pdf')
+    render('points.Rmd')
+    render('points.Rmd', 'tufte_handout')
+    file.rename('points.pdf', 'points_book.pdf')
 
     #render('sp_eco.Rmd')
     #render('sp_eco.Rmd', 'tufte_handout')
